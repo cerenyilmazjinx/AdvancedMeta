2175ccc47ecf0aba79bb781f3be489dc36525cc7
Cereny <56936296+cerenyilmazjinx@users.noreply.github.com>
2025-08-02 21:44:25 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/advancedmeta/main.go
@@ -0,0 +1,23 @@
+// cmd/advancedmeta/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"advancedmeta/internal/advancedmeta"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := advancedmeta.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
